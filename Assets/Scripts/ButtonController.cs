da0d6c94decbf78aa58babcda0f4a68eb4fe360d
MartinPlascencia <martin.plascencia@live.com.mx>
2025-10-28 18:55:55 -0600

    Implementada funcionalidad boton arma.

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+public class ButtonController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField]
+    public UnityEvent onButtonDown;
+    [SerializeField]
+    public UnityEvent onButtonUp;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        onButtonDown?.Invoke();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        onButtonUp?.Invoke();
+    }
+}
